77ff7c12ab9d97b6579936093098e50017aea7e6
Shadi Akiki <shadiakiki1986@gmail.com>
2018-04-06 04:43:30 +0300

    fix download script

--- 46-box-plots-for-education-reboot/download.sh
+++ 46-box-plots-for-education-reboot/download.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-wget -qO- https://s3.amazonaws.com/drivendata/data/4/public/da1dd36a-a497-42c7-b3f3-4a225944bdba.zip | unzip -d data_in
+if [ -f files.zip ]; then
+    echo "already downloaded"
+    exit 1
+fi
+wget -O files.zip https://s3.amazonaws.com/drivendata/data/4/public/da1dd36a-a497-42c7-b3f3-4a225944bdba.zip && unzip files.zip -d data_in
